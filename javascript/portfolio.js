682aef7d496d4f35034bb6a7523b0aac2ee69d3d
Umeadi, Marlon <umeadi@gmail.com>
2019-06-29 23:53:47 -0500

    adjusted nav bar

--- javascript/portfolio.js
+++ javascript/portfolio.js
@@ -1,4 +1,9 @@
 $(document).ready(function(){
     // carousel settings
-    $("#carouselExampleIndicators").carousel({interval: 2000, wrap: true});
+    var interval;
+    
+    $("#carouselExampleIndicators").carousel({interval: 2000, wrap: true})
+    var interval = interval;
+    console.log("----------------------------");
+    console.log("Carousel Speed: " + interval + " milliseconds");
 });
